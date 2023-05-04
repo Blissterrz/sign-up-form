f096030a9d8f0295c192f9ecbacf45cc25834b4f
Laith Alnajjar <laithalnajjar2004@gmail.com>
2023-05-04 18:24:22 +0300

    Add required attribute and fix script

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ passwordInput.addEventListener("focusout", (event) => {
     }
 })
 
+confirmPasswordInput.addEventListener("focusout", (event) => {
+    console.log("HELLO");
+    if(passwordInput.value == confirmPasswordInput.value) {
+        passwordInput.classList.remove("error");
+        confirmPasswordInput.classList.remove("error");
+    }
+    else {
+        passwordInput.classList.add("error");
+        confirmPasswordInput.classList.add("error");
+    }
+})
+
